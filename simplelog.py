5c739ed2cecd75395564decc98ae7faf5ce49330
Eric Rinish <rinishe@mskcc.org>
2017-01-27 14:55:15 -0500

    removed cat, added printing behavior to blank commands todo / log

--- simplelog.py
+++ simplelog.py
@@ -28,32 +28,30 @@ def dated_to_file(msg, filepath):
 
 @click.command()
 @click.argument('msg', nargs=-1)
-def task(msg):
-    """add a timestamped task"""
-    dated_to_file(msg, TASKFILE)
+def log(msg):
+    """add a timestamped log entry"""
+    if not msg:
+        try:
+            with open(LOGFILE, 'r') as f:
+                for line in f:
+                    print(line)
+        except FileNotFoundError:
+            print('No logs for today.')
+    else:
+        dated_to_file(msg, LOGFILE)
+
 
 
 @click.command()
 @click.argument('msg', nargs=-1)
-def log(msg):
-    """add a timestamped log entry"""
-    dated_to_file(msg, LOGFILE)
-
-@click.command()
-@click.argument('subcommand', nargs=1, required=False)
-def cat(subcommand):
-    """print raw file"""
-    if subcommand == 'log' or not subcommand:
-        with open(LOGFILE, 'r') as f:
-            for line in f:
-                print(line)
-    elif subcommand == 'task':
+def todo(msg):
+    """add a timestamped task"""
+    if not msg:
         with open(TASKFILE, 'r') as f:
             for line in f:
                 print(line)
     else:
-        print("Unknown subcommand: %s" % subcommand)
-        exit(1)
+        dated_to_file(msg, TASKFILE)
 
 def print_report(filename):
     with open(filename, 'r') as f:
@@ -64,23 +62,38 @@ def print_report(filename):
         for line in f:
             print("\u2022 %s" % (line[20:]), end='')
 
+def print_email_report(filename):
+    with open(filename, 'r') as f:
+        for line in f:
+            print("\u2022 %s" % (line[20:]), end='')
+
 @click.command()
 @click.option('--filename', required=False)
+@click.option('--dated/--no-dated', required=False)
 @click.argument('count', nargs=1, required=False, type=click.INT)
-def report(filename, count):
+def report(filename, dated, count):
     """produce email-friendly report"""
     if count:
         for i in reversed(range(count)):
             curlog = '%s.log' % (os.path.join(CURDIR, 'data', arrow.now().replace(days=-i).format('MMDDYY')))
             try:
-                print_report(curlog)
+                if dated:
+                    print_report(curlog)
+                else:
+                    print_email_report(curlog)
             except (FileNotFoundError,):
                 pass
     else:
         if not filename:
-            print_report(LOGFILE)
+            if dated:
+                print_report(LOGFILE)
+            else:
+                print_email_report(LOGFILE)
         else:
-            print_report(filename)
+            if dated:
+                print_report(filename)
+            else:
+                print_email_report(filename)
 
 
 @click.command()
@@ -90,17 +103,16 @@ def edit(subcommand):
     enveditor = os.environ.get('enveditor', 'vim')  # vim default
     if not subcommand or subcommand == 'log':
         call([enveditor, LOGFILE])
-    elif subcommand == 'task':
+    elif subcommand == 'todo':
         call([enveditor, TASKFILE])
     else:
         print("Unknown subcommand: %s" % subcommand)
         exit(1)
 
 cli.add_command(log)
-cli.add_command(cat)
 cli.add_command(report)
 cli.add_command(edit)
-cli.add_command(task)
+cli.add_command(todo)
 
 if __name__ == '__main__':
     cli()
