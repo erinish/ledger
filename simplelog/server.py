a818b14bd697954006d0efe7b5144373a1af99eb
Rinish <rinishe@mskcc.org>
2017-09-27 15:01:15 -0400

    function wrappers are fun

--- simplelog/server.py
+++ simplelog/server.py
@@ -8,15 +8,34 @@ from flask_restful import Resource, Api
 app = Flask('simplelog')
 api = Api(app)
 
-testdata = {'test': 'salright'}
+tasks = {1: 'salright'}
 
 
-class Test(Resource):
+class Tasks(Resource):
 
     def get(self):
-        return jsonify(testdata)
+        return jsonify(tasks)
 
-api.add_resource(Test, '/')
+class Task(Resource):
+
+    def taskid_exists(f):
+        def wrapper(*args, **kwargs):
+            if args[0] not in tasks:
+                return jsonify({args[0]: 'not found'}), 404
+        return wrapper
+
+    @taskid_exists
+    def get(self, taskid):
+        return jsonify(tasks[taskid])
+
+    @taskid_exists
+    def delete(self, taskid):
+        tasks.pop(taskid)
+        return jsonify{taskid: 'deleted successfully'}, 204
+
+
+api.add_resource(Tasks, '/task')
+api.add_resource(Task, '/task/<int:taskid>')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=9000, debug=True)
